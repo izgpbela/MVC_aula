50f5258e987e20a523627f2de0185f05048647bf
Izgpbela <izgpbela.dev@gmail.com>
2024-09-09 20:45:54 -0300

    atualização de head

--- view/head.php
+++ view/head.php
@@ -5,3 +5,4 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 </head>
+
